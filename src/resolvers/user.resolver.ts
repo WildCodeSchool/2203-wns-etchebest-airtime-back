7686564d6cf7b6378c2b908147e22a0a8f08237d
YoannBoisson <yoann.boisson@live.fr>
2022-05-19 15:01:24 +0200

    CRUD for ticket and user

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -4,4 +4,18 @@ module.exports = {
   Query: {
     getAllUsers: async () => await UserModel.findAllUsers(),
   },
+  Mutation: {
+    createUser: async (_: any, args: any) => {
+      return await UserModel.createUser(args);
+    },
+    deleteUser: async (_: any, args: any) => {
+      return await UserModel.deleteUser(args);
+    },
+    updateUser: async (_: any, args: any) => {
+      return await UserModel.updateUser({
+        id: args.id,
+        newAttributes: args,
+      });
+    },
+  },
 };
