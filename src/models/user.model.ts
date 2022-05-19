bde785e6be2aa56d82825b9593cefce12d69621b
YoannBoisson <yoann.boisson@live.fr>
2022-05-19 15:01:24 +0200

    CRUD for ticket and user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,77 @@
-const db = require('../config/db_config');
+export const database = require('../config/db_config');
+
+interface IUser {
+  id?: string;
+  firstname?: string;
+  lastname?: string;
+  email?: number;
+  password?: number;
+  role?: string;
+}
 
 const findAllUsers = async () => {
-    const result = await db.promise().query("SELECT BIN_TO_UUID(id) id, firstname, lastname, email, password, role FROM user");
-    return result[0];
-}
+  const result = await database.promise().query('SELECT * FROM user');
+  return result[0];
+};
+
+const createUser = async ({
+  firstname,
+  lastname,
+  email,
+  password,
+  role,
+}: IUser) => {
+  await database
+    .promise()
+    .query(
+      `INSERT INTO user (id, firstname, lastname, email, password, role) VALUES (UUID(), ?, ?, ?, ?, ?)`,
+      [firstname, lastname, email, password, role]
+    );
+  const res = await database
+    .promise()
+    .query('SELECT * FROM user WHERE email = ?', [email]);
+
+  return res[0][0];
+};
+
+const deleteUser = async ({ id }: { id: string }) => {
+  const res = await database
+    .promise()
+    .query('SELECT * FROM user WHERE id = ?', [id]);
+  const resTicket = await database
+    .promise()
+    .query('SELECT * FROM ticket WHERE user_id = ?', [id]);
+  if (resTicket[0].length !== 0) {
+    await database
+      .promise()
+      .query('UPDATE ticket SET user_id = null WHERE user_id = ?', [id]);
+  }
+  await database.promise().query('DELETE FROM user WHERE id = ?', [id]);
+
+  return res[0][0];
+};
+
+const updateUser = async ({
+  id,
+  newAttributes,
+}: {
+  id: number;
+  newAttributes: IUser;
+}) => {
+  console.log(newAttributes);
+  await database
+    .promise()
+    .query('UPDATE user SET ? WHERE id = ?', [newAttributes, id]);
+
+  const res = await database
+    .promise()
+    .query('SELECT * FROM user WHERE id = ?', [id]);
+  return res[0][0];
+};
 
 module.exports = {
-    findAllUsers
-}
+  findAllUsers,
+  createUser,
+  deleteUser,
+  updateUser,
+};
--- src/resolvers/ticket.resolver.ts
+++ src/resolvers/ticket.resolver.ts
@@ -4,4 +4,18 @@ module.exports = {
   Query: {
     getAllTickets: async () => await TicketModel.findAllTickets(),
   },
+  Mutation: {
+    createTicket: async (_: any, args: any) => {
+      return await TicketModel.createTicket(args);
+    },
+    deleteTicket: async (_: any, args: any) => {
+      return await TicketModel.deleteTicket(args);
+    },
+    updateTicket: async (_: any, args: any) => {
+      return await TicketModel.updateTicket({
+        id: args.id,
+        newAttributes: args,
+      });
+    },
+  },
 };
